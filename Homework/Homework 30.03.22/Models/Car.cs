9ff06a62ea121e0e9ea7f8fccee012ca44421353
Hatamli <kamilahat@code.edu.az>
2022-03-30 04:27:12 +0400

    homework added to GitHub repo

---
+++ Homework/Homework 30.03.22/Models/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_30._03._22.Models
+{
+    class Car : Vehicle
+    {
+        public double km;
+        public double liter;
+        private double _consumption;
+        public double Consumption
+        {
+            get
+            {
+                return _consumption;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _consumption = value;
+                }
+            }
+        }
+        public Car(double consumption)
+        {
+            _consumption = consumption;
+        }
+
+        public double Drive(double consumption, double liter, double km)
+        {
+            
+            return km;
+        }
+        public double Drive()
+        {
+
+        }
+    }
+}
---
+++ Homework/Homework 30.03.22/Models/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_30._03._22.Models
+{
+    class Vehicle
+    {
+        private string _brand;
+        private string _model;
+        private int _tankCapacity;
+        private double _engine;
+        private int _maxSpeed;
+
+        public string Brand { get { return _brand; } set { _brand = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public int TankCapacity
+        {
+            get
+            {
+                return _tankCapacity;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _tankCapacity = value;
+                }
+            }
+        }
+
+        public double Engine
+        {
+            get
+            {
+                return _engine;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _engine = value;
+                }
+            }
+        }
+        public int MaxSpeed
+        {
+            get
+            {
+                return _maxSpeed;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _maxSpeed = value;
+                }
+            }
+        }
+
+        public Vehicle(string brand, string model, int tankCapacity, double engine, int maxSpeed)
+        {
+            _brand = brand;
+            _model = model;
+            _tankCapacity = tankCapacity;
+            _engine = engine;
+            _maxSpeed = maxSpeed;
+        }
+    }
+}
+
